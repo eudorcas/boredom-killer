5643211f9e31cb7edfd66524aa04b249e1c43366
Klaudia Butkiewicz <klaudia.butkiewicz09@gmail.com>
2018-12-16 19:47:10 +0100

    Icons

--- src/components/FormType.js
+++ src/components/FormType.js
@@ -40,43 +40,43 @@ class FormType extends React.Component {
                         style={{color: this.myColor(0, "purple"), borderColor: this.myColor(0, "purple")}}
                         className={"button"}
                         onClick={e => this.handleClick(0, "recreating")}>
-                        recreation
+                        recreation <i className="fas fa-bowling-ball"></i>
                     </div>
                     <div
                         style={{color: this.myColor(1, "gray"), borderColor: this.myColor(1, "gray")}}
                         className={"button"}
                         onClick={e => this.handleClick(1, "education")}>
-                        education
+                        education <i className="fas fa-graduation-cap"></i>
                     </div>
                     <div
                         style={{color: this.myColor(2, "green"), borderColor: this.myColor(2, "green")}}
                         className={"button"}
                         onClick={e => this.handleClick(2, "relaxation")}>
-                        relax
+                        relax <i className="fas fa-spa"></i>
                     </div>
                     <div
                         style={{color: this.myColor(3, "plum"), borderColor: this.myColor(3, "plum")}}
                         className={"button"}
                         onClick={e => this.handleClick(3, "social")}>
-                        social
+                        social <i className="fas fa-glass-cheers"></i>
                     </div>
                     <div
                         style={{color: this.myColor(4, "white"), borderColor: this.myColor(4, "white")}}
                         className={"button"}
                         onClick={e => this.handleClick(4, "charity")}>
-                        charity
+                        charity <i className="fas fa-hand-holding-heart"></i>
                     </div>
                     <div
                         style={{color: this.myColor(5, "red"), borderColor: this.myColor(5, "red")}}
                         className={"button"}
                         onClick={e => this.handleClick(5, "busywork")}>
-                        busywork
+                        busywork <i className="fas fa-briefcase"></i>
                     </div>
                     <div
                         style={{color: this.myColor(6, "orange"), borderColor: this.myColor(6, "orange")}}
                         className={"button"}
                         onClick={e => this.handleClick(6, "cooking")}>
-                        cooking
+                        cooking <i className="fas fa-utensils"></i>
                     </div>
                 </div>
             </div>
